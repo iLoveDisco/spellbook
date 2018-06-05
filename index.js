f09a62128f05ae6b75044fee52c50e6d0a034958
iLoveDisco <erictu32@yahoo.com>
2018-06-04 20:51:59 -0400

    Finished: Everything but the last bonus

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("testing")
+const textChangeButton = document.querySelector('button');
+const heading = document.querySelector('#dictHeading');
+const form = document.querySelector('form');
+
+const submission = document.querySelector('#submission');
+
+function changeText () {
+    heading.textContent = submission.value;
+}
+
+function changeTextUsingEnter (event) {
+    if (event.keyCode == 13) {
+        heading.textContent = submission.value;
+    }
+}
+
+textChangeButton.addEventListener('click', changeText);
+form.addEventListener('keypress', changeTextUsingEnter);
