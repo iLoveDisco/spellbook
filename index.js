2670afd09ee41c109a92ed0c87da89802c5a70f6
iLoveDisco <erictu32@yahoo.com>
2018-06-05 18:21:44 -0400

    This commit will add a color feature and uses better syntax

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const addSpellButton = document.querySelector('button');
 const form = document.querySelector('form');
+const spellList = document.querySelector('#spells');
 
-const submission = document.querySelector('#submission');
+const elementSelection = document.querySelector('select');
 
-function changeText (event) {
+function addSpell (event) {
     event.preventDefault();
-    heading.textContent = submission.value;
-    form.fname.value = "";
+    if (form.spellName.value != ""){// no empty entries allowed
+        applyAppendage(form.spellName.value, elementSelection.value);
+    }
+    form.spellName.value = "";
 }
 
-form.addEventListener('submit', changeText);
+function applyAppendage (spell, element) {
+    const listItem = document.createElement('li');
+    listItem.textContent = spell + ' - ' + element;
+    switch (element) {
+        case 'Fire':
+            listItem.style.color = 'Red';
+            break;
+        case 'Water':
+            listItem.style.color = 'Blue';
+            break;
+        case 'Earth':
+            listItem.style.color = 'Brown';
+            break;
+        case 'Air':
+            listItem.style.color = 'White';
+            break;
+    }
+    spellList.appendChild(listItem);
+}
+
+form.addEventListener('submit', addSpell);
