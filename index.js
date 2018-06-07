a9fadbcb6e37e021060dce0d67495f49a17e1884
iLoveDisco <erictu32@yahoo.com>
2018-06-07 15:42:03 -0400

    Initial hw4 commit

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 function allowDrop(ev) {
     ev.preventDefault();
 }
-var entriesDatabase = [];
 var entryList = document.querySelector('#spells');
 const app = {
     init: function() {
+        this.entriesDatabase = [];
+        this.template = document.querySelector('.location.template')
+        
         const form = document.querySelector('form');
         form.addEventListener('submit', ev => {
-            this.addEntry(ev)
+            ev.preventDefault();
+            this.handleSubmit(ev);
         })
     },
-    addEntry: function(ev) {
-        ev.preventDefault();
-        const form = document.querySelector('form');
+    handleSubmit: function(ev) {
+        const form = ev.target; // the form
         if (form.nameOfPlace.value != ""){ // no empty entries allowed
             this.appendToList(form.nameOfPlace, form.descriptionOfPlace);
         }
@@ -28,29 +30,51 @@ const app = {
             nameOfPlace: place.value,
             desc: description.value
         }
+        this.entriesDatabase.push(entry); // pushes onto database
 
-        const el = this.renderElement (entry);
-        entriesDatabase.push(el);
+        const el = this.renderItem (entry);
+
 
         const delButton = document.createElement('button');
         delButton.textContent = "Remove";
         listItem.appendChild(delButton);
         delButton.type = "remove";
 
-        delButton.addEventListener('click', ev => {
-            entryList.removeChild(listItem);
-
-        })
+        delButton.addEventListener('click', this.removeSpell.bind(this, entry));
 
         listItem.appendChild(el);
         entryList.appendChild(listItem);
     },
-    renderElement: function(entry) {
-        const el = document.createElement('span');
-        const text = document.createElement('span');
-        text.textContent = `   ${entry['nameOfPlace']} - ${entry['desc']}`;
-        el.appendChild(text);
-        return el;
+
+    removeEntry: function(entry, ev) {
+        entryList.removeChild(listItem);
+        
+        let i = entriesDatabase.indexOf(entry);
+        this.entriesDatabase.splice(i,1);
+    },
+
+    renderItem: function(entry) {
+        const item = this.template.cloneNode(true);
+        item.classList.remove('template') // helps hide the initial template
+
+        // ['name', 'desc']
+        const properties = Object.keys(entry);
+        properites.forEach(property => {
+            const el = item.querySelector(`.${property}`);
+            el.textContent = entry[property];
+            el.setAttribute('title', spell[property]);
+        })
+
+        // delete button
+        item
+            .querySelector('button.delete')
+            .addEventListener('click', this.removeEntry.bind(this,spell));
+
+        // const el = document.createElement('span');
+        // const text = document.createElement('span');
+        // text.textContent = `   ${entry['nameOfPlace']} - ${entry['desc']}`;
+        // el.appendChild(text);
+        // return el;
     }
 }
 
