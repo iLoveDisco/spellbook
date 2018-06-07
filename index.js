503167c04508d122a9738da44606f7722f3a8560
iLoveDisco <erictu32@yahoo.com>
2018-06-07 13:32:38 -0400

    Final commit: This commit will add delete buttons that remove items from list and array

--- index.js
+++ index.js
@@ -2,22 +2,24 @@
 function allowDrop(ev) {
     ev.preventDefault();
 }
-
+var entriesDatabase = [];
+var entryList = document.querySelector('#spells');
 const app = {
     init: function() {
         const form = document.querySelector('form');
-        const entryList = document.querySelector('#spells');
         form.addEventListener('submit', ev => {
             this.addEntry(ev)
         })
     },
     addEntry: function(ev) {
         ev.preventDefault();
-        if (this.form.nameOfPlace.value != ""){ // no empty entries allowed
-            appendToList(form.nameOfPlace, form.descriptionOfPlace);
+        const form = document.querySelector('form');
+        if (form.nameOfPlace.value != ""){ // no empty entries allowed
+            this.appendToList(form.nameOfPlace, form.descriptionOfPlace);
         }
         form.nameOfPlace.value = "";
-        form.descriptionOfPlace = "";
+        form.descriptionOfPlace.value = "";
+        form.nameOfPlace.focus();
     },
     appendToList: function(place, description) {
         const listItem = document.createElement('li');
@@ -26,15 +28,28 @@ const app = {
             nameOfPlace: place.value,
             desc: description.value
         }
-        const el = renderElement (entry);
+
+        const el = this.renderElement (entry);
+        entriesDatabase.push(el);
+
+        const delButton = document.createElement('button');
+        delButton.textContent = "Remove";
+        listItem.appendChild(delButton);
+        delButton.type = "remove";
+
+        delButton.addEventListener('click', ev => {
+            entryList.removeChild(listItem);
+
+        })
 
         listItem.appendChild(el);
         entryList.appendChild(listItem);
     },
     renderElement: function(entry) {
         const el = document.createElement('span');
-        el.textContent = `${entry['nameOfPlace']} - ${entry['desc']}`;
-        el.className = "entry";
+        const text = document.createElement('span');
+        text.textContent = `   ${entry['nameOfPlace']} - ${entry['desc']}`;
+        el.appendChild(text);
         return el;
     }
 }
